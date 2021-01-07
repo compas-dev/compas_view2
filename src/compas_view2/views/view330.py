55b0077e5742bcf604b140c0330947a8726a9348
Tom Van Mele <vanmelet@ethz.ch>
2021-01-07 13:51:41 +0100

    base 330 view

--- src/compas_view2/views/view330.py
+++ src/compas_view2/views/view330.py
@@ -3,12 +3,39 @@ from OpenGL import GL
 
 from PySide2 import QtCore, QtWidgets
 
-from ..camera import Camera
-from ..mouse import Mouse
 from ..shaders import Shader
-
 from .view import View
 
 
 class View330(View):
-    pass
+    """View widget for OpenGL 3.3 and GLSL 330 and above, with a Core Profile.
+    """
+
+    def init(self):
+        # init the buffers
+        for guid in self.objects:
+            obj = self.objects[guid]
+            obj.init()
+        # create the program
+        self.shader = Shader()
+        self.shader.bind()
+        self.shader.uniform4x4(
+            "projection", self.camera.projection(self.app.width, self.app.height))
+        self.shader.uniform4x4("viewworld", self.camera.viewworld())
+        self.shader.uniform1i("is_selected", 0)
+        self.shader.uniform1f("opacity", self.opacity)
+        self.shader.uniform3f("selection_color", self.selection_color)
+        self.shader.release()
+
+    def resize(self, w: int, h: int):
+        self.shader.bind()
+        self.shader.uniform4x4("projection", self.camera.projection(w, h))
+        self.shader.release()
+
+    def paint(self):
+        self.shader.bind()
+        self.shader.uniform4x4("viewworld", self.camera.viewworld())
+        for guid in self.objects:
+            obj = self.objects[guid]
+            obj.draw(self.shader)
+        self.shader.release()
