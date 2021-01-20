3f445c6fed91d26594550e836a9e1555b6c2cd71
tomvanmele <van.mele@arch.ethz.ch>
2021-01-20 17:46:20 +0100

    grid with axes

--- src/compas_view2/objects/gridobject.py
+++ src/compas_view2/objects/gridobject.py
@@ -26,22 +26,47 @@ class GridObject(Object):
         color = self.default_color_edges
         n = 0
 
-        for x in range(- self.x_cells, self.x_cells+1):
-            positions.append([x*self.cell_size, -self.x_cells*self.cell_size, 0])
-            positions.append([x*self.cell_size, self.x_cells*self.cell_size, 0])
-            colors.append(color)
-            colors.append(color)
-            n = len(elements)
-            elements.extend([n, n+1])
+        for x in range(- self.x_cells, self.x_cells + 1):
+            if x == 0:
+                positions.append([x * self.cell_size, -self.x_cells * self.cell_size, 0])
+                positions.append([x * self.cell_size, 0, 0])
+                colors.append(color)
+                colors.append(color)
+                positions.append([x * self.cell_size, 0, 0])
+                positions.append([x * self.cell_size, self.x_cells * self.cell_size, 0])
+                colors.append([0, 1, 0])
+                colors.append([0, 1, 0])
+                n = len(elements)
+                elements.extend([n + 0, n + 1])
+                elements.extend([n + 2, n + 3])
+            else:
+                positions.append([x * self.cell_size, -self.x_cells * self.cell_size, 0])
+                positions.append([x * self.cell_size, self.x_cells * self.cell_size, 0])
+                colors.append(color)
+                colors.append(color)
+                n = len(elements)
+                elements.extend([n, n + 1])
 
-        for y in range(- self.y_cells, self.y_cells+1):
-            positions.append([-self.y_cells*self.cell_size, y*self.cell_size, 0])
-            positions.append([self.y_cells*self.cell_size, y*self.cell_size, 0])
-            colors.append(color)
-            colors.append(color)
-            n = len(elements)
-            elements.extend([n, n+1])
-
+        for y in range(- self.y_cells, self.y_cells + 1):
+            if y == 0:
+                positions.append([-self.y_cells * self.cell_size, y * self.cell_size, 0])
+                positions.append([0, y * self.cell_size, 0])
+                colors.append(color)
+                colors.append(color)
+                positions.append([0, y * self.cell_size, 0])
+                positions.append([self.y_cells * self.cell_size, y * self.cell_size, 0])
+                colors.append([1, 0, 0])
+                colors.append([1, 0, 0])
+                n = len(elements)
+                elements.extend([n + 0, n + 1])
+                elements.extend([n + 2, n + 3])
+            else:
+                positions.append([-self.y_cells * self.cell_size, y * self.cell_size, 0])
+                positions.append([self.y_cells * self.cell_size, y * self.cell_size, 0])
+                colors.append(color)
+                colors.append(color)
+                n = len(elements)
+                elements.extend([n, n + 1])
 
         make_vertex_buffer(list(flatten(positions)))
 
