ae2ddabbcbdc20b3c3f903625eac6839c6c312c4
tomvanmele <van.mele@arch.ethz.ch>
2021-02-01 22:05:18 +0100

    add dynamic access option

--- src/compas_view2/buffers.py
+++ src/compas_view2/buffers.py
@@ -2,23 +2,49 @@ import ctypes as ct
 from OpenGL import GL
 
 
-def make_vertex_buffer(data):
+def make_vertex_buffer(data, dynamic=False):
+    """Make a vertex buffer from the given data.
+
+    Parameters
+    ----------
+    data: list[float]
+        A flat list of floats.
+
+    Returns
+    -------
+    int
+        Vertex buffer ID.
+    """
+    access = GL.GL_DYNAMIC_DRAW if dynamic else GL.GL_STATIC_DRAW
     n = len(data)
     size = n * ct.sizeof(ct.c_float)
     vbo = GL.glGenBuffers(1)
     data = (ct.c_float * n)(* data)
     GL.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo)
-    GL.glBufferData(GL.GL_ARRAY_BUFFER, size, data, GL.GL_STATIC_DRAW)
+    GL.glBufferData(GL.GL_ARRAY_BUFFER, size, data, access)
     GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
     return vbo
 
 
-def make_index_buffer(data):
+def make_index_buffer(data, dynamic=False):
+    """Make an element buffer from the given data.
+
+    Parameters
+    ----------
+    data: list[int]
+        A flat list of ints.
+
+    Returns
+    -------
+    int
+        Element buffer ID.
+    """
+    access = GL.GL_DYNAMIC_DRAW if dynamic else GL.GL_STATIC_DRAW
     n = len(data)
     size = n * ct.sizeof(ct.c_uint)
     vbo = GL.glGenBuffers(1)
     data = (ct.c_int * n)(* data)
     GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, vbo)
-    GL.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, size, data, GL.GL_STATIC_DRAW)
+    GL.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, size, data, access)
     GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, 0)
     return vbo
