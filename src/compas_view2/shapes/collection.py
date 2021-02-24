54230cc3fa8f0bc035b6a156f05e60863ad3fbb6
Li <leochen1990@gmail.com>
2021-02-24 14:40:24 +0100

    use `items` instead of `shapes`

--- src/compas_view2/shapes/collection.py
+++ src/compas_view2/shapes/collection.py
@@ -1,23 +1,22 @@
-from compas.geometry import Shape
 from compas.datastructures import Mesh
-# import numpy as np
+
 
 __all__ = ['Collection']
 
 
-class Collection(Shape):
-    """A collection of shapes
+class Collection(Mesh):
+    """A collection of items(inheritances of compas.datastructures.Mesh)
     """
 
-    def __init__(self, shapes=[]):
+    def __init__(self, items=None):
         super().__init__()
-        self.shapes = shapes
+        self.items = items or []
 
     def to_mesh(self, color=None, colors=None):
-        colors = colors or [color or self.default_color_faces] * len(self.shapes)
+        colors = colors or [color or self.default_color_faces] * len(self.items)
         mesh = Mesh()
-        for shape, color in zip(self.shapes, colors):
-            v, f = shape.to_vertices_and_faces()
+        for item, color in zip(self.items, colors):
+            v, f = item.to_vertices_and_faces()
             m = Mesh.from_vertices_and_faces(v, f)
             for f in m.faces():
                 m.face_attribute(f, 'color', color)
