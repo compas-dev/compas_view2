57cdd35ef9d79f616265df15ad52c9e2fbbdd8cd
brgcode <vanmelet@ethz.ch>
2021-03-23 15:47:08 +0100

    authors

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ autosummary_generate = True
 
 # napoleon options
 
-napoleon_google_docstring = True
+napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
