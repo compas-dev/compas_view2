82c242884f406ebbec4728e140fbfb858a3b0773
brgcode <vanmelet@ethz.ch>
2021-05-11 21:40:41 +0200

    show edges true

--- src/compas_view2/objects/bufferobject.py
+++ src/compas_view2/objects/bufferobject.py
@@ -26,7 +26,7 @@ class BufferObject(Object):
     default_color_faces = [0.8, 0.8, 0.8]
 
     def __init__(self, data, name=None, is_selected=False,
-                 show_points=False, show_vertices=False, show_lines=False, show_edges=False, show_faces=True,
+                 show_points=False, show_vertices=False, show_lines=False, show_edges=True, show_faces=True,
                  pointcolor=None, linecolor=None, facecolor=None,
                  linewidth=None, pointsize=None, opacity=1):
         super().__init__(data, name=name, is_selected=is_selected)
