9f80e1d0ad3da3c6ad610789bef418a5c70ff9b9
brgcode <vanmelet@ethz.ch>
2021-05-17 11:33:15 +0200

    load config file as python

--- src/compas_view2/app/__init__.py
+++ src/compas_view2/app/__init__.py
@@ -1,2 +1,8 @@
+import json
+import compas
+
 from .controller import Controller  # noqa: F401
 from .app import App  # noqa: F401
+
+with open(compas.here(__file__, 'config.json')) as f:
+    config = json.load(f)
