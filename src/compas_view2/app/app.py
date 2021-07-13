e699f574facfd679e59dfee2023ff5928d3cc6c4
brgcode <vanmelet@ethz.ch>
2021-07-13 16:12:36 +0200

    add decorators for ui objects

--- src/compas_view2/app/app.py
+++ src/compas_view2/app/app.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from typing import AnyStr, Callable, Optional, List, Dict, Any
+from typing_extensions import Literal
+
 import sys
 import os
 import json
@@ -5,6 +10,8 @@ import json
 from functools import partial
 
 from PySide2 import QtCore, QtGui, QtWidgets
+
+from compas.data import Data
 
 from ..views import View120
 from ..views import View330
@@ -15,10 +22,10 @@ from .selector import Selector
 from .timer import Timer
 
 HERE = os.path.dirname(__file__)
-ICONS = os.path.join(HERE, '../icons')
-CONFIG = os.path.join(HERE, 'config.json')
+ICONS = os.path.join(HERE, "../icons")
+CONFIG = os.path.join(HERE, "config.json")
 
-VERSIONS = {'120': (2, 1), '330': (3, 3)}
+VERSIONS = {"120": (2, 1), "330": (3, 3)}
 
 
 class App:
@@ -35,20 +42,20 @@ class App:
 
     Parameters
     ----------
-    version: '120' | '330', optional
+    version: "120" | "330", optional
         The version of the GLSL used by the shaders.
-        Default is ``'120'`` with a compatibility profile.
-        The option ``'330'`` is not yet available.
+        Default is ``"120"`` with a compatibility profile.
+        The option ``"330"`` is not yet available.
     width: int, optional
         The width of the app window at startup.
         Default is ``800``.
     height: int, optional
         The height of the app window at startup.
         Default is ``500``.
-    viewmode: 'shaded' | 'ghosted' | 'wireframe' | 'lighted', optional
+    viewmode: "shaded" | "ghosted" | "wireframe" | "lighted", optional
         The display mode of the OpenGL view.
-        Default is ``'shaded'``.
-        In ``'ghosted'`` mode, all objects have a default opacity of ``0.7``.
+        Default is ``"shaded"``.
+        In ``"ghosted"`` mode, all objects have a default opacity of ``0.7``.
     show_grid: bool, optional
         Show the XY plane.
         Default is ``True``.
@@ -83,7 +90,7 @@ class App:
 
     Currently the app has no scene graph.
     All added COMPAS objects are wrapped in a viewer object and stored in a dictionary,
-    mapping the object's ID (``id(object)``) to the instance.
+    mapping the object"s ID (``id(object)``) to the instance.
 
     Examples
     --------
@@ -93,17 +100,26 @@ class App:
 
     """
 
-    def __init__(self, version='120', width=800, height=500, viewmode='shaded', controller_class=None, show_grid=True, config=None):
+    def __init__(self,
+                 version: Literal["120", "330"] = "120",
+                 width: int = 800,
+                 height: int = 500,
+                 viewmode: Literal["wireframe", "shaded", "ghosted", "lighted"] = "shaded",
+                 controller_class: Optional[Controller] = None,
+                 show_grid: bool = True,
+                 config: Optional[dict] = None,
+                 enable_sidebar: bool = False):
+
         if version not in VERSIONS:
             raise Exception("Only these versions are currently supported: {}".format(VERSIONS))
 
         glFormat = QtGui.QSurfaceFormat()
         glFormat.setVersion(* VERSIONS[version])
 
-        if version == '330':
+        if version == "330":
             View = View330
             glFormat.setProfile(QtGui.QSurfaceFormat.CoreProfile)
-        elif version == '120':
+        elif version == "120":
             View = View120
             glFormat.setProfile(QtGui.QSurfaceFormat.CompatibilityProfile)
         else:
@@ -143,8 +159,8 @@ class App:
         self._app.references.add(self.window)
         self.selector = Selector(self)
 
+        self.enable_sidebar = enable_sidebar
         self.init()
-
         self.resize(width, height)
 
     def init(self):
@@ -153,7 +169,7 @@ class App:
         self._init_toolbar(self.config.get("toolbar"))
         self._init_sidebar(self.config.get("sidebar"))
 
-    def resize(self, width, height):
+    def resize(self, width: int, height: int):
         """Resize the main window programmatically.
 
         Parameters
@@ -168,7 +184,7 @@ class App:
         y = 0.5 * (rect.height() - height)
         self.window.setGeometry(x, y, width, height)
 
-    def add(self, data, **kwargs):
+    def add(self, data: Data, **kwargs) -> Object:
         """Add a COMPAS object.
 
         Parameters
@@ -186,14 +202,23 @@ class App:
             obj.init()
         return obj
 
-    def remove(self, obj):
+    def add_reference(self, obj: Object, **kwargs) -> Object:
+        """"""
+        ref = obj.otype.from_other(obj, **kwargs)
+        self.view.objects[ref] = ref
+        self.selector.add(ref)
+        if self.view.isValid():
+            ref.init()
+        return ref
+
+    def remove(self, obj: Object):
         if obj in list(self.view.objects):
             del self.view.objects[obj]
         for key, value in list(self.selector.instances.items()):
             if obj == value:
                 del self.selector.instances[key]
 
-    def show(self):
+    def show(self, pause=None):
         """Show the viewer window."""
         self.window.show()
         self._app.exec_()
@@ -202,37 +227,37 @@ class App:
 
     def about(self):
         """Display the about message as defined in the config file."""
-        QtWidgets.QMessageBox.about(self.window, 'About', self.config['messages']['about'])
+        QtWidgets.QMessageBox.about(self.window, "About", self.config["messages"]["about"])
 
-    def info(self, message):
+    def info(self, message: str):
         """Display info."""
-        QtWidgets.QMessageBox.information(self.window, 'Info', message)
+        QtWidgets.QMessageBox.information(self.window, "Info", message)
 
-    def question(self, message):
+    def question(self, message: str):
         """Ask a question."""
         pass
 
-    def warning(self, message):
+    def warning(self, message: str):
         """Display a warning."""
-        QtWidgets.QMessageBox.warning(self.window, 'Warning', message)
+        QtWidgets.QMessageBox.warning(self.window, "Warning", message)
 
-    def critical(self, message):
+    def critical(self, message: str):
         """Display a critical warning."""
-        QtWidgets.QMessageBox.critical(self.window, 'Critical', message)
+        QtWidgets.QMessageBox.critical(self.window, "Critical", message)
 
-    def status(self, message):
+    def status(self, message: str):
         """Display a message in the status bar."""
         self.statusText.setText(message)
 
-    def fps(self, _fps):
+    def fps(self, fps: int):
         """Update fps info in the status bar."""
-        self.statusFps.setText("fps: {}".format(_fps))
+        self.statusFps.setText("fps: {}".format(fps))
 
     # ==============================================================================
     # UI
     # ==============================================================================
 
-    def _get_icon(self, icon):
+    def _get_icon(self, icon: str):
         return QtGui.QIcon(os.path.join(ICONS, icon))
 
     def _init_statusbar(self):
@@ -243,7 +268,7 @@ class App:
         self.statusFps = QtWidgets.QLabel("fps: ")
         self.statusbar.addWidget(self.statusFps)
 
-    def _init_menubar(self, items):
+    def _init_menubar(self, items: List[Dict]):
         if not items:
             return
         self.menubar = self.window.menuBar()
@@ -251,107 +276,240 @@ class App:
         self.menubar.setContentsMargins(0, 0, 0, 0)
         self._add_menubar_items(items, self.menubar)
 
-    def _init_toolbar(self, items):
+    def _init_toolbar(self, items: List[Dict]):
         if not items:
             return
-        self.toolbar = self.window.addToolBar('Tools')
+        self.toolbar = self.window.addToolBar("Tools")
         self.toolbar.setMovable(False)
-        self.toolbar.setObjectName('Tools')
+        self.toolbar.setObjectName("Tools")
         self.toolbar.setIconSize(QtCore.QSize(16, 16))
         self._add_toolbar_items(items, self.toolbar)
 
-    def _init_sidebar(self, items):
-        if not items:
+    def _init_sidebar(self, items: List[Dict]):
+        if not self.enable_sidebar:
             return
         self.sidebar = QtWidgets.QToolBar(self.window)
         self.window.addToolBar(QtCore.Qt.LeftToolBarArea, self.sidebar)
-        self.sidebar.setObjectName('Sidebar')
+        self.sidebar.setObjectName("Sidebar")
         self.sidebar.setMovable(False)
         self.sidebar.setIconSize(QtCore.QSize(16, 16))
         self.sidebar.setMinimumWidth(240)
-        palette = self.sidebar.palette()
-        palette.setColor(QtGui.QPalette.Window, QtGui.QColor(255, 0, 0))
-        self.sidebar.setPalette(palette)
-        self.sidebar.setAutoFillBackground(True)
-        self._add_menubar_items(items, self.sidebar)
+        self._add_sidebar_items(items, self.sidebar)
 
-    def _add_menubar_items(self, items, parent):
+    def _add_menubar_items(self, items: List[Dict], parent: QtWidgets.QWidget):
         if not items:
             return
         for item in items:
-            if item['type'] == 'separator':
+            if item["type"] == "separator":
                 parent.addSeparator()
-            elif item['type'] == 'menu':
-                menu = parent.addMenu(item['text'])
-                if 'items' in item:
-                    self._add_menubar_items(item['items'], menu)
-            elif item['type'] == 'radio':
+            elif item["type"] == "menu":
+                menu = parent.addMenu(item["text"])
+                if "items" in item:
+                    self._add_menubar_items(item["items"], menu)
+            elif item["type"] == "radio":
                 radio = QtWidgets.QActionGroup(self.window, exclusive=True)
-                for item in item['items']:
-                    action = self._add_action(item, parent)
+                for item in item["items"]:
+                    action = self._add_action(parent, text=item["text"], action=item["action"])
                     action.setCheckable(True)
-                    action.setChecked(item['checked'])
+                    action.setChecked(item["checked"])
                     radio.addAction(action)
-            elif item['type'] == 'slider':
-                self._add_slider(item, parent)
-            elif item['type'] == 'action':
-                self._add_action(item, parent)
+            elif item["type"] == "action":
+                del item["type"]
+                self._add_action(parent, text=item["text"], action=item["action"])
             else:
                 raise NotImplementedError
 
-    def _add_toolbar_items(self, items, parent):
+    def _add_toolbar_items(self, items: List[Dict], parent: QtWidgets.QWidget):
         if not items:
             return
         for item in items:
-            if item['type'] == 'separator':
+            if item["type"] == "separator":
                 parent.addSeparator()
-            elif item['type'] == 'action':
-                self._add_action(item, parent)
-            elif item['type'] == 'slider':
-                self._add_slider(item, parent)
+            elif item["type"] == "action":
+                del item["type"]
+                self._add_action(parent, **item)
             else:
                 raise NotImplementedError
 
-    def _add_action(self, item, parent):
-        text = item['text']
-        action = item['action'] if callable(item['action']) else getattr(self.controller, item['action'])
-        args = item.get('args', None) or []
-        kwargs = item.get('kwargs', None) or {}
-        if 'icon' in item:
-            icon = self._get_icon(item['icon'])
-            return parent.addAction(icon, text, partial(action, *args, **kwargs))
-        return parent.addAction(text, partial(action, *args, **kwargs))
+    def _add_sidebar_items(self, items: List[Dict], parent: QtWidgets.QWidget):
+        if not items:
+            return
+        for item in items:
+            if item["type"] == "separator":
+                parent.addSeparator()
+            elif item["type"] == "radio":
+                del item["type"]
+                self.add_radio(parent, **item)
+            elif item["type"] == "checkbox":
+                del item["type"]
+                self.add_checkbox(parent, **item)
+            elif item["type"] == "slider":
+                del item["type"]
+                self.add_slider(parent, **item)
+            elif item["type"] == "button":
+                del item["type"]
+                self.add_button(parent, **item)
+            else:
+                raise NotImplementedError
 
-    def _add_slider(self, item, parent):
+    def _add_action(self,
+                    parent: QtWidgets.QWidget,
+                    *,
+                    text: str,
+                    action: Callable,
+                    args: Optional[List[Any]] = None,
+                    kwargs: Optional[Dict] = None,
+                    icon: Optional[AnyStr] = None):
+        action = action if callable(action) else getattr(self.controller, action)
+        args = args or []
+        kwargs = kwargs or {}
+        if icon:
+            icon = self._get_icon(icon)
+            action = parent.addAction(icon, text, partial(action, *args, **kwargs))
+        else:
+            action = parent.addAction(text, partial(action, *args, **kwargs))
+        return action
+
+    def add_button(self,
+                   parent: QtWidgets.QWidget,
+                   *,
+                   text: str,
+                   action: Callable):
         box = QtWidgets.QWidget()
-        box_layout = QtWidgets.QHBoxLayout()
-        title = QtWidgets.QLabel(item.get("title", ""))
-        value = QtWidgets.QLabel(str(item.get("value", 0)))
-        label = QtWidgets.QLabel(str(item.get("label", "")))
+        layout = QtWidgets.QHBoxLayout()
+        button = QtWidgets.QPushButton(text)
+        layout.addWidget(button)
+        box.setLayout(layout)
+        parent.addWidget(box)
+        action = action if callable(action) else getattr(self.controller, action)
+        button.clicked.connect(action)
+        # button.clicked.connect(self.view.update)
+
+    def add_radio(self,
+                  parent: QtWidgets.QWidget,
+                  *,
+                  items: List[Dict]):
+        box = QtWidgets.QWidget()
+        layout = QtWidgets.QHBoxLayout()
+        radio = QtWidgets.QActionGroup(self.window, exclusive=True)
+        layout.addWidget(radio)
+        box.setLayout(layout)
+        parent.addWidget(box)
+        for item in items:
+            action = self._add_action(parent, text=item["text"], action=item["action"])
+            action.setCheckable(True)
+            action.setChecked(item["checked"])
+            radio.addAction(action)
+        # radio.toggled.connect(self.view.update)
+
+    def add_checkbox(self,
+                     parent: QtWidgets.QWidget,
+                     *,
+                     text: str,
+                     action: Callable,
+                     checked: bool = False):
+        box = QtWidgets.QWidget()
+        layout = QtWidgets.QHBoxLayout()
+        checkbox = QtWidgets.QCheckBox(text)
+        checkbox.setCheckState(QtCore.Qt.CheckState.Checked if checked else QtCore.Qt.CheckState.Unchecked)
+        layout.addWidget(checkbox)
+        box.setLayout(layout)
+        parent.addWidget(box)
+        action = action if callable(action) else getattr(self.controller, action)
+        checkbox.toggled.connect(action)
+        checkbox.toggled.connect(self.view.update)
+
+    def add_input(self, parent: QtWidgets.QWidget):
+        pass
+
+    def add_colorpicker(self, parent: QtWidgets.QWidget):
+        pass
+
+    def add_slider(self,
+                   parent: QtWidgets.QWidget,
+                   *,
+                   text: str,
+                   action: Callable,
+                   value: int = 0,
+                   minval: int = 0,
+                   maxval: int = 100,
+                   step: int = 1,
+                   interval: int = 1,
+                   label: str = ""):
+        box = QtWidgets.QWidget()
+        layout = QtWidgets.QHBoxLayout()
+        value_label = QtWidgets.QLabel(str(value))
         slider = QtWidgets.QSlider()
         slider.setOrientation(QtCore.Qt.Horizontal)
-        slider.setValue(item.get("value", 0))
-        slider.setMinimum(item.get("min", 0))
-        slider.setMaximum(item.get("max", 100))
-        slider.setTickInterval(item.get("interval", 1))
-        slider.setSingleStep(item.get("step", 1))
-        box_layout.addWidget(title)
-        box_layout.addWidget(slider)
-        box_layout.addWidget(value)
-        box_layout.addWidget(label)
-        box.setLayout(box_layout)
+        slider.setValue(value)
+        slider.setMinimum(minval)
+        slider.setMaximum(maxval)
+        slider.setTickInterval(interval)
+        slider.setSingleStep(step)
+        layout.addWidget(QtWidgets.QLabel(text))
+        layout.addWidget(slider)
+        layout.addWidget(value_label)
+        layout.addWidget(QtWidgets.QLabel(str(label)))
+        box.setLayout(layout)
         parent.addWidget(box)
-        slider.valueChanged.connect(lambda v: value.setText(str(v)))
-        if "action" in item:
-            action = item['action'] if callable(item['action']) else getattr(self.controller, item['action'])
-            slider.valueChanged.connect(action)
+        slider.valueChanged.connect(lambda v: value_label.setText(str(v)))
+        action = action if callable(action) else getattr(self.controller, action)
+        slider.valueChanged.connect(action)
         slider.valueChanged.connect(self.view.update)
 
-    def on(self, interval=None, timeout=None, record=False, frames=None, record_path="temp/out.gif", playback_interval=None):
+    # ==============================================================================
+    # Decorators
+    # ==============================================================================
+
+    def button(self, text: str) -> Callable:
+        def outer(func: Callable) -> Callable:
+            def wrapped(*args, **kwargs):
+                func(self.app, *args, **kwargs)
+            self.add_button(self.sidebar, text=text, action=func)
+            return wrapped
+        return outer
+
+    def checkbox(self, text: str, checked: bool = True) -> Callable:
+        def outer(func: Callable) -> Callable:
+            def wrapped(*args, **kwargs):
+                func(self.app, *args, **kwargs)
+            self.add_checkbox(self.sidebar, text=text, action=func, checked=checked)
+            return wrapped
+        return outer
+
+    def slider(self,
+               text: str,
+               value: int = 0,
+               minval: int = 0,
+               maxval: int = 100,
+               step: int = 1,
+               label: str = "") -> Callable:
+        def outer(func: Callable) -> Callable:
+            def wrapped(*args, **kwargs):
+                func(self.app, *args, **kwargs)
+            self.add_slider(self.sidebar,
+                            text=text,
+                            value=value,
+                            minval=minval,
+                            maxval=maxval,
+                            step=step,
+                            label=label,
+                            action=func)
+            return wrapped
+        return outer
+
+    def on(self,
+           interval: int = None,
+           timeout: int = None,
+           record: bool = False,
+           frames: int = None,
+           record_path: str = "temp/out.gif",
+           playback_interval: int = None) -> Callable:
+
         if (not interval and not timeout) or (interval and timeout):
             raise ValueError("Must specify either interval or timeout")
 
-        def outer(func):
+        def outer(func: Callable):
             def render():
                 func(self.frame_count)
                 self.view.update()
