c5233563707ab48ad27d4c82c3b8001e84d352c4
brgcode <van.mele@arch.ethz.ch>
2022-01-19 15:07:40 +0100

    test script

--- scripts/v120_ui_elements.py
+++ scripts/v120_ui_elements.py
@@ -4,7 +4,10 @@ from compas_view2.app import App
 
 curve = Bezier([[0, 0, 0], [3, 6, 0], [5, -3, 0], [10, 0, 0]])
 
-viewer = App(viewmode="ghosted", enable_sidebar=True)
+viewer = App(viewmode="shaded", enable_sidebar=True, width=1600, height=900)
+viewer.view.camera.tx = -5.0
+viewer.view.camera.rz = 0
+viewer.view.camera.rx = -20
 
 pointobj = viewer.add(Point(* curve.point(0)), size=20, color=(1, 0, 0))
 curveobj = viewer.add(Polyline(curve.locus()), linewidth=2)
@@ -25,12 +28,23 @@ def slide(value):
 
 
 @viewer.button(text="Reset")
-def click(checked):
+def click():
     if viewer.confirm('This will reset the point to parameter t=0.'):
         pointobj._data = curve.point(0)
         pointobj.update()
-        slide.slider.setValue(0)
+        slide.value = 0
         viewer.view.update()
+
+
+@viewer.radio(title='Display', items=[
+    {'text': 'Ghosted', 'value': 'ghosted', 'checked': viewer.view.mode == 'ghosted'},
+    {'text': 'Shaded', 'value': 'shaded', 'checked': viewer.view.mode == 'shaded'},
+    {'text': 'Lighted', 'value': 'lighted', 'checked': viewer.view.mode == 'lighted'},
+    {'text': 'Wireframe', 'value': 'wireframe', 'checked': viewer.view.mode == 'wireframe'}
+])
+def select(value):
+    viewer.view.mode = value
+    viewer.view.update()
 
 
 viewer.run()
