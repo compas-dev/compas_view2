383577d62e9a93063deaf2d35adf5c849c250992
brgcode <van.mele@arch.ethz.ch>
2022-02-02 22:35:41 +0100

    add select element

--- src/compas_view2/ui/__init__.py
+++ src/compas_view2/ui/__init__.py
@@ -2,3 +2,4 @@ from .button import Button  # noqa : F401
 from .slider import Slider  # noqa : F401
 from .radio import Radio  # noqa : F401
 from .checkbox import Checkbox  # noqa : F401
+from .select import Select  # noqa : F401
