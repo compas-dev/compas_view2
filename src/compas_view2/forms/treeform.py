2e9d1c9cadc6af802d4443de3d679d03ac632034
Licini <leochen1990@gmail.com>
2022-07-15 16:34:52 +0200

    allow change cell color

--- src/compas_view2/forms/treeform.py
+++ src/compas_view2/forms/treeform.py
@@ -1,5 +1,6 @@
 from .dockform import DockForm
 from qtpy import QtWidgets
+from qtpy import QtGui
 
 
 class TreeForm(DockForm):
@@ -11,20 +12,22 @@ class TreeForm(DockForm):
         self.tree.setColumnCount(2)
         self.tree.setHeaderLabels(["Key", "Value"])
         self.setWidget(self.tree)
-    
+
     def update(self, entries):
         self.tree.clear()
         self.map_entries(entries)
 
     def map_entries(self, entries, parent=None):
-
         for entry in entries:
-            children = entry.get("children", [])
+            children = entry.get("children")
             if children:
-                item = QtWidgets.QTreeWidgetItem(entry["key"])
+                item = QtWidgets.QTreeWidgetItem([entry["key"]])
                 self.map_entries(children, item)
             else:
-                item = QtWidgets.QTreeWidgetItem([entry["key"], str(entry["value"])])
+                item = QtWidgets.QTreeWidgetItem([entry["key"], str(entry.get("value", ""))])
+
+            if entry.get("color"):
+                item.setBackgroundColor(1, QtGui.QColor(*entry.get("color")))
 
             if parent is not None:
                 parent.addChild(item)
