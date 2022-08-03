2e35bceea95e1666164dd241f6a02faf8a4bb90c
Li Chen <leochen1990@gmail.com>
2022-08-03 16:25:03 +0200

    add line representation and end effector

--- scripts/v120_robot.py
+++ scripts/v120_robot.py
@@ -1,17 +1,22 @@
 from compas.robots import GithubPackageMeshLoader
 from compas.robots import RobotModel
 from compas_view2.app import App
+from compas.geometry import Box
+from compas.geometry import Frame
 
 github = GithubPackageMeshLoader('ros-industrial/abb', 'abb_irb6600_support', 'kinetic-devel')
 model = RobotModel.from_urdf_file(github.load_urdf('irb6640.urdf'))
 model.load_geometry(github)
-# model = RobotModel.ur5(True)
+# model = RobotModel.ur5(False)
 
 viewer = App(viewmode="lighted", enable_sceneform=True, enable_sidebar=True, width=2000, height=1000)
 
 robotObj = viewer.add(model)
 
-for joint_name in robotObj.joints:
+end_effector_link_obj = robotObj.link_objs[model.get_end_effector_link_name()]
+end_effector_link_obj.add(Box(Frame([0, 0, 0.25], [1, 0, 0], [0, 1, 0]), 0.2, 0.2, 0.5), name="end_effector_dummy_box")
+
+for joint_name in model.get_configurable_joint_names():
 
     @viewer.slider(title=joint_name, minval=-180 , maxval=180)
     def rotate(angle, joint_name=joint_name):
--- src/compas_view2/objects/robotobject.py
+++ src/compas_view2/objects/robotobject.py
@@ -1,14 +1,18 @@
 from compas_view2.collections import Collection
+from compas_view2.shapes import Arrow
 
 from .collectionobject import CollectionObject
 
 import itertools
-
+from compas.geometry import Line
+from compas.geometry import Plane
+from compas.geometry import Circle
 from compas.geometry import Transformation
 from compas.geometry import Translation
 from compas.geometry import Rotation
 from compas.robots import Geometry
 from math import radians
+
 
 class RobotObject(CollectionObject):
     """Object for displaying COMPAS RobotModel."""
@@ -17,6 +21,7 @@ class RobotObject(CollectionObject):
         super().__init__(Collection(), name=robot.name, **kwargs)
         self.robot = robot
         self.joints = {}
+        self.link_objs = {}
         self.create(robot.root, self)
 
     def create(self, link, parent, parent_joint=None):
@@ -25,22 +30,33 @@ class RobotObject(CollectionObject):
 
         for item in itertools.chain(link.visual):
             meshes.extend(Geometry._get_item_meshes(item))
-        
-        obj = parent.add(Collection(meshes), name=link.name, show_edges=False)
+
+        if meshes:
+            obj = parent.add(Collection(meshes), name=link.name, show_edges=False)
+        else:
+            lines = []
+            for joint in link.joints:
+                line = Line([0, 0, 0], joint.origin.point)
+                lines.append(line)
+                if parent_joint:
+                    lines.append(Circle(Plane([0, 0, 0], parent_joint.axis.vector), line.length/3))
+            obj = parent.add(Collection(lines), name=link.name)
 
         if parent_joint:
             obj.matrix = Transformation.from_frame(parent_joint.origin).matrix
             self.joints[parent_joint.name] = {
-                'linkObj': obj,
+                'link_obj': obj,
                 'joint': parent_joint,
                 'axis': parent_joint.axis.vector
             }
 
+        self.link_objs[link.name] = obj
+
         for joint in link.joints:
-            self.create(joint.child_link, parent = obj, parent_joint=joint)
-    
+            self.create(joint.child_link, parent=obj, parent_joint=joint)
+
     def rotate_joint(self, joint_name, angle):
         joint = self.joints[joint_name]
-        T = Translation.from_vector(joint['linkObj'].translation)
+        T = Translation.from_vector(joint['link_obj'].translation)
         R = Rotation.from_axis_and_angle(joint['axis'], radians(angle))
-        joint['linkObj'].matrix = (T * R).matrix
+        joint['link_obj'].matrix = (T * R).matrix
