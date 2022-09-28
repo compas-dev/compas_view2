0fdf90bab016462438a85594fe04f04d246c043b
Li Chen <leochen1990@gmail.com>
2022-09-28 13:19:54 +0200

    fix mac tabs display issue

--- src/compas_view2/forms/tabsform.py
+++ src/compas_view2/forms/tabsform.py
@@ -6,12 +6,15 @@ from qtpy import QtWidgets
 class TabsForm(DockForm):
     def __init__(self, app, title="Tree", tabs=[], columns=["key", "value"], show_headers=True):
         super().__init__(app, title)
-
+        containerwidget = QtWidgets.QWidget()
         tabwidget = QtWidgets.QTabWidget()
         for tab in tabs:
             print(tab["name"])
             treeForm = TreeForm(app, title=tab["name"], data=tab["data"], columns=columns, show_headers=show_headers)
             treeForm.setTitleBarWidget(QtWidgets.QWidget())
             tabwidget.addTab(treeForm, tab["name"])
-
-        self.setWidget(tabwidget)
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(tabwidget)
+        containerwidget.setLayout(layout)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setWidget(containerwidget)
